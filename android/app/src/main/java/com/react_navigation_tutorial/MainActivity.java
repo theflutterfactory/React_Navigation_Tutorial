e296113f46d59e84d537cb777faa7bcc577ca806
Julian Currie <julian.currie@gmail.com>
2019-04-30 12:31:45 +0700

    added react navigation and gesture handler library

--- android/app/src/main/java/com/react_navigation_tutorial/MainActivity.java
+++ android/app/src/main/java/com/react_navigation_tutorial/MainActivity.java
@@ -1,6 +1,9 @@
 package com.react_navigation_tutorial;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,14 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "React_Navigation_Tutorial";
     }
+    
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+  }
 }
