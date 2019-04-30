6bb28cf3384382b4d36a4298cd7b5a220a8424fd
Julian Currie <julian.currie@gmail.com>
2019-04-30 13:09:40 +0700

    added stackNavigator and SwitchNavigator with routes

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import Login from './src/components/Auth/Login';
+import Screen1 from './src/components/DrawerScreens/Screen1';
+import Screen2 from './src/components/DrawerScreens/Screen1';
+import Screen3 from './src/components/DrawerScreens/Screen1';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Login />
-    );
+import Login from './src/components/Auth/Login';
+import Detail from './src/components/Detail';
+import Feed from './src/components/Feed';
+
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+
+const AppStack = createStackNavigator({
+  FeedRoute: Feed,
+  DetailRoute: Detail
+});
+
+const AuthStack = createStackNavigator({
+  LoginRoute: Login
+});
+
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'Auth',
   }
-}
+));
